9480d0b6cd1847051daf058110cdcabe29c036d8
marshon03 <greenmarshon03@gmail.com>
2020-09-20 22:32:56 -0400

    Update MLPClassifier.py

--- MLPClassifier.py
+++ MLPClassifier.py
@@ -16,8 +16,8 @@ import matplotlib.pyplot as plt
 import re
 import string
 
-data = pd.read_csv('/Users/marshongreen/Documents/GATech ML(7641)/Datasets/IMDB Dataset.csv')
-game_data = pd.read_csv('/Users/marshongreen/Documents/GATech ML(7641)/Datasets/games.csv')
+data = pd.read_csv('Add IMDB data location here')
+game_data = pd.read_csv('Add chess games data location here')
 
 
 def custom_preprocessor(text):
